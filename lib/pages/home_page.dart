c2a776dfda9512192f19889317bd87ca6ccd1b3d
Pepito2024 <021370b@utea.edu.pe>
2023-11-15 15:17:49 -0500

    #en proceso

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_codigo_youapp/ui/general/colors.dart';
+import 'package:flutter_codigo_youapp/ui/widgets/item_filter_widget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 14,
+          vertical: 0,
+        ),
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              child: Row(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      primary: kBrandSecondaryColor,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                    ),
+                    icon: const Icon(
+                      Icons.explore_outlined,
+                    ),
+                    label: const Text(
+                      "Explorar",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                    child: VerticalDivider(
+                      color: Colors.white30,
+                      thickness: 1.0,
+                    ),
+                  ),
+                  ItemFilterWidget(
+                    text: "Todos",
+                    isSelected: true
+                  ),
+                  ItemFilterWidget(
+                    text: "Mixes",
+                    isSelected: false
+                  ),
+                  ItemFilterWidget(
+                    text: "Musica",
+                    isSelected: false
+                  ),
+                  ItemFilterWidget(
+                    text: "Programacion",
+                    isSelected: false
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/init_page.dart
+++ lib/pages/init_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_youapp/pages/home_page.dart';
 import 'package:flutter_codigo_youapp/ui/general/colors.dart';
 
 class InitPage extends StatefulWidget {
@@ -12,7 +13,7 @@ class _InitPageState extends State<InitPage> {
   int _currenIndex = 0;
 
   List<Widget> _pages = [
-    Center(child: Text("Principal")),
+    HomePage(),
     Center(child: Text("Shorts")),
     Center(child: Text("Agregar")),
     Center(child: Text("Suscripciones")),
